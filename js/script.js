b0f3eb27500a8dcb07e4db0be11438090c2b9b28
doldol123 <toruig59@gmail.com>
2022-09-30 17:25:18 -0700

    pseudocode script with basic structure

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//declare variables to use via querySelector
+//declare a string array to store all quiz questions
+//declare a string array to store all answers
+
+
+// use addEventListener to setup click functions for starting the quiz to the start quiz button
+
+//setup a function to store "High scores"
+    //when click High score on upper-left jump high score section
+    //show high score by decresing order with user initial and scores
+    //Go back and Clear high score function
+
+//create a interval timer couting down
+    //when timer hits 0, end quiz
+        //show user "All Done!" on screen
+        //show the final score 
+        //setup an input textarea to let user input intials
+        //use addEventListsener to the submit button 
+        //create a function when submit button call the function to storage initials to local storage
+        
+//after quiz start
+    //random pick a question to display on the screen with 4 answers
+        //all the answers have to be setup to a button 
+            //when click the answer's button 
+                //compare the correct answer with user choice
+                    //if answer is true total score +1
+                    //if answer is wrong total time decrese -15
+        //setup a function to show if the answer is correct
+    //if user finish the quiz before time counts to 0
+        //*show user "All Done!" on screen
+        //*show the final score 
+        //*setup an input textarea to let user input intials
+        //*use addEventListsener to the submit button 
+        //*create a function when submit button call the function to storage initials to local storage
+        // * parts are same function as timer hits 0 end quiz
+
+//
+
+
